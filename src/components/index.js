29e9a6aa711c16a0239afade362c4ed573c42e3e
Maheswarha S.Rajagopal <maheswarharajagopal@gmail.com>
2021-05-07 23:54:21 +0800

    First commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Navbar } from "./navbar/navbar";
+export { default as Products } from "./products/products";
+export { default as Cart } from "./cart/cart";
+export { default as Checkout } from "./checkout-form/checkout/checkout";
